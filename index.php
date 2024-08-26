24b0e3ed392c207d2698529fd2bb64d4e6a4cc70
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-08-26 18:57:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello World!<br>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet">
+    <title>Test</title>
+</head>
+
+<body class="bg-sky-700 flex flex-col items-center justify-center min-h-screen">
+    <button class="px-4 bg" onclick="window.location.href='https://www.google.com'">Visit Google</button>
+</body>
+
+</html>
